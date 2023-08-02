359e9a707df0efee0e17579d76a0c22d9fe6ceba
lzpel <18492524+lzpel@users.noreply.github.com>
2023-08-02 21:20:03 +0900

    Add assetPrefix: process.env.REPOSITORY_NAME

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export'//https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
+    output: 'export',//https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
+    assetPrefix: process.env.REPOSITORY_NAME,
 }
 
 module.exports = nextConfig
