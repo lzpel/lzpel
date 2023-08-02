e05b193f304eb17ef128985192a7f2184cd6bf6c
lzpel <18492524+lzpel@users.noreply.github.com>
2023-08-02 21:24:51 +0900

    fix next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',//https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
-    assetPrefix: process.env.REPOSITORY_NAME,
+}
+if(process.env.REPOSITORY_NAME){
+    nextConfig.assetPrefix="/"+process.env.REPOSITORY_NAME
 }
 
 module.exports = nextConfig
