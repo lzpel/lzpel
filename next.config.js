3cef70530c171a2dc881fe7e13ec2e2d5b126819
lzpel <18492524+lzpel@users.noreply.github.com>
2023-08-02 21:29:22 +0900

    test

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',//https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
-}
-if(process.env.REPOSITORY_NAME){
-    nextConfig.assetPrefix="/"+process.env.REPOSITORY_NAME
+    assetPrefix: "/lzpel"
 }
 
 module.exports = nextConfig
