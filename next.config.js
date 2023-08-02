bac108be18469da3255e4e205bb0e77d8791a016
lzpel <18492524+lzpel@users.noreply.github.com>
2023-08-02 21:38:38 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
+const urlPrefix = '/lzpel'
 const nextConfig = {
     output: 'export',//https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
-    assetPrefix: "/lzpel"
+    assetPrefix: urlPrefix,
+    basePath: urlPrefix,
 }
 
 module.exports = nextConfig
