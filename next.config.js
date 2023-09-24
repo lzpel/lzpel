f72fed3eb4331f02bf17b058d98541d467521568
lzpel <18492524+lzpel@users.noreply.github.com>
2023-09-24 19:38:03 +0900

    reset

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const urlPrefix = '/lzpel'
-const nextConfig = {
-    output: 'export',//https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
-    assetPrefix: urlPrefix,
-    basePath: urlPrefix,
-}
-
-module.exports = nextConfig
