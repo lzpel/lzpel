fa02aedf2b0a1f5e1388956e7aa05b60aaaec5d0
lzpel <18492524+lzpel@users.noreply.github.com>
2023-10-14 09:45:11 +0900

    20231014

--- src/ui/Markdown.tsx
+++ src/ui/Markdown.tsx
@@ -4,6 +4,7 @@ import ReactMarkdown from "react-markdown";
 import rehypeRaw from "rehype-raw";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
+import url from "@/utils/url";
 
 const Markdown = (props: { path: string }) => {
   const markdownPath = path.join(process.cwd(), props.path);
@@ -12,6 +13,15 @@ const Markdown = (props: { path: string }) => {
     <ReactMarkdown
       rehypePlugins={[rehypeRaw, rehypeKatex]}
       remarkPlugins={[remarkMath]}
+      components={{
+        img: (props) => {
+          if (props.src && props.src.startsWith("/")) {
+            const new_props = { ...props, src: url(props.src) };
+            return <img {...new_props} />;
+          }
+          return <img {...props} />;
+        },
+      }}
     >
       {markdown}
     </ReactMarkdown>
