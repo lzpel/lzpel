18c03e959ed5fd31323c7e85745db18e307b5bed
lzpel <18492524+lzpel@users.noreply.github.com>
2023-10-30 22:52:23 +0900

    fix

--- src/utils/CustomMetadata.ts
+++ src/utils/CustomMetadata.ts
@@ -2,7 +2,7 @@ import url from "@/utils/url";
 import { Metadata } from "next";
 
 const CustomMetadata = (title?: string, description?: string): Metadata => {
-  const BaseTitle = "浮体自己構築型海上都市";
+  const BaseTitle = "自己組織型海上都市";
   const Title = title ? `${title} - ${BaseTitle}` : BaseTitle;
   const Description = description ?? "Floating Offshore Self-constructing City";
   return {
